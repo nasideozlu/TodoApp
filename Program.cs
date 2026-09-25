56bddb5509857d4eb31d8f822e22686dbe94c140
naside <ozlunaside03@gmail.com>
2026-09-25 08:17:05 +0300

    Ilk commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+List<Todo> todos = new List<Todo>();
+int nextId = 1;
+bool shouldExit = false;
+while (!shouldExit)
+{
+Console.WriteLine("Yapmak Istediginiz Islemi Secin");
+Console.WriteLine("1.Gorevleri Listele");
+Console.WriteLine("2.Gorev Ekle");
+Console.WriteLine("3.Gorev Sil");
+Console.WriteLine("4.Cikis Yap");
+
+var kullaniciSecimi = Console.ReadLine();
+
+switch(kullaniciSecimi)
+{
+case "1":
+    Console.WriteLine("Mevcut Gorevler:");
+    
+    foreach(Todo gorev in todos)
+    {
+         Console.WriteLine($"Id: {gorev.Id} , Gorev: {gorev.Name} , Tamamlandi: {gorev.Completed}");
+    }
+    break;
+
+case "2":
+    Console.WriteLine("Gorev adini yaziniz:");
+    var gorevAdi = Console.ReadLine();
+    Todo yeniGorev = new Todo();
+    yeniGorev.Id = nextId;
+    yeniGorev.Name = gorevAdi;
+    yeniGorev.Completed = false;
+    todos.Add(yeniGorev);
+    nextId++;
+    break;
+
+case"3":
+    Console.WriteLine("Silmek istediginiz gorevi seciniz:");
+    
+    int id = Convert.ToInt32(Console.ReadLine());
+    Todo? silinecek = null;
+
+    foreach( Todo gorev in todos)
+    {
+        if (gorev.Id == id)
+        {
+            silinecek = gorev;
+        }
+    
+    }
+    if (silinecek != null)
+    {
+         todos.Remove(silinecek);
+    }
+    break;
+
+case "4":
+    Console.WriteLine("Cikis Yapiliyor.");
+    shouldExit = true;
+    break;
+
+default:
+    Console.WriteLine("Hatali Secim Yaptiniz");
+    break;
+}
+
+}
---
+++ Todo.cs
@@ -0,0 +1,6 @@
+public class Todo 
+{
+public int Id {get; set;}
+public string Name {get; set;}
+public bool Completed {get; set;}  
+}
